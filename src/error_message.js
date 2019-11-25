8d3615f8905e26f6dc5f9137321e54dc2a3d9339
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2019-11-25 18:01:21 +0530

    sophisticated error message added

--- src/error_message.js
+++ src/error_message.js
@@ -1,6 +1,11 @@
 const errorMessage = function() {
-  const message = "error!!!";
-  return message;
+  const message1 =
+    "usage:-\nfor query : node beverage.js --query --empId employeeId\n";
+  const message2 =
+    "for save  : node beverage.js --save --(options) --(details)...{3}\n";
+  const message3 = "\t    options =>    --empId,   --beverage, --qty\n";
+  const message4 = "\t    details => employeeId, beverageName, quantity";
+  return message1 + message2 + message3 + message4;
 };
 
 exports.errorMessage = errorMessage;
--- test/test_saveTransaction_function.js
+++ test/test_saveTransaction_function.js
@@ -4,13 +4,16 @@ const assert = require("assert");
 
 describe("save", function() {
   const line1 = "Transaction recorded:\nEmployee ID,Beverage,Quantity,Date\n";
-  const sampAndPath = { stamp: new Date(), path: "./test/.tmp.txt" };
+  const sampAndPath = {
+    stamp: new Date(),
+    path: "./test/.fake_database_save.txt"
+  };
   const testInput = [["11111", "banana", "1"]];
 
   it("should display the deatails of the enterd employee", function() {
     const actual = save.apply(sampAndPath, testInput);
     const expected = line1 + "11111,banana,1," + sampAndPath.stamp.toJSON();
     assert.strictEqual(actual, expected);
-    fs.writeFileSync("test/.tmp.txt", "", "utf8");
+    fs.writeFileSync("test/.fake_database_save.txt", "", "utf8");
   });
 });
