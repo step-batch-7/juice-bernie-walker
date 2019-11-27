acacca3fb101a88f7c24ac6918746724c9e37478
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2019-11-27 13:39:59 +0530

    refactored test_record_modfier.js
    * removed .test_samples.json.
    * included all the test samples required in the test_record_modfier.js.
    
    * the test_record_modfier.js was reading and writing from the
    test_samples.json file.
    
    * testing will be a lot faster and test file will be a lot easier to
    apprehend.

--- test/test_record_modifier.js
+++ test/test_record_modifier.js
@@ -3,25 +3,38 @@ const assert = require("assert");
 const fs = require("fs");
 
 describe("recordModifier", function() {
-  const testSampleFile = fs.readFileSync("./test/.test_samples.JSON", "utf8");
+  const testObjectString =
+    '{"25318":[{"employeeId":"25318", "beverage":"grape", "quantity":1, "date":"2019-11-20T05:50:28.267Z"}]}';
+  const insertionTestExpected =
+    '{"25314": [{"employeeId":"25314", "beverage":"mango", "quantity": 2, "date": "2019-11-20T05:50:28.267Z"}],\
+    "25318":[{"employeeId":"25318","beverage":"grape","quantity":1,"date":"2019-11-20T05:50:28.267Z"}]}';
+  const updationTestExpected =
+    '{"25318":[{"employeeId":"25318","beverage":"grape","quantity":1,"date":"2019-11-20T05:50:28.267Z"},\
+    {"employeeId":"25318","beverage":"banana","quantity":1,"date":"2019-11-20T05:50:28.267Z"}]}';
 
   it("should insert new record into given object if it doesn't already exist", function() {
-    const testSamples = JSON.parse(testSampleFile);
-    let testObject = testSamples.testObject;
-    const testId = testSamples.insertionTestInput.id;
-    const testEntry = testSamples.insertionTestInput.entry;
+    let testObject = JSON.parse(testObjectString);
+    const testId = "25314";
+    const testEntry = {
+      beverage: "mango",
+      quantity: 2,
+      date: "2019-11-20T05:50:28.267Z"
+    };
+    const expected = JSON.parse(insertionTestExpected);
     recordModifier(testObject, testId, testEntry);
-    const expected = testSamples.insertionTestExpected;
     assert.deepStrictEqual(testObject, expected);
   });
 
   it("should update the existing record", function() {
-    const testSamples = JSON.parse(testSampleFile);
-    let testObject = testSamples.testObject;
-    const testId = testSamples.updationTestInput.id;
-    const testEntry = testSamples.updationTestInput.entry;
+    let testObject = JSON.parse(testObjectString);
+    const testId = "25318";
+    const testEntry = {
+      beverage: "banana",
+      quantity: 1,
+      date: "2019-11-20T05:50:28.267Z"
+    };
+    const expected = JSON.parse(updationTestExpected);
     recordModifier(testObject, testId, testEntry);
-    const expected = testSamples.updationTestExpected;
     assert.deepStrictEqual(testObject, expected);
   });
 });
